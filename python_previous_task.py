d5caa7c77f92865855b50ea7ac7de1ed34a091fc
svydra <tancor23@mail.ru>
2020-04-17 19:34:28 +0300

    Refactored python_previous_task.py by PEP8

--- python_previous_task.py
+++ python_previous_task.py
@@ -4,8 +4,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 
+
 def calc(x):
     return str(math.log(abs(12 * math.sin(int(x)))))
+
 
 browser = webdriver.Chrome()
 
@@ -14,8 +16,8 @@ try:
 
     # говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
     WebDriverWait(browser, 20).until(
-            EC.text_to_be_present_in_element((By.ID, "price"), "$100")
-        )
+        EC.text_to_be_present_in_element((By.ID, "price"), "$100")
+    )
     button = browser.find_element_by_id("book")
     button.click()
 
